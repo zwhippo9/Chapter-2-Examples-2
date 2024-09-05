e8d85bf87da252e5e2e46ec9f60828064eedf252
zwhippo9 <zwhippo9@gmail.com>
2024-09-05 20:12:03 +0000

    Example 2 Test 1

--- .ccls-cache/@home@runner@Chapter-2-Examples/main.cpp
+++ .ccls-cache/@home@runner@Chapter-2-Examples/main.cpp
@@ -1,23 +1,18 @@
-//This program calculates and displays the several values 
-// and various percentages for those values
-// Calculating Percentages
+// This program calculates and displays the discounted value of an item.
+// Percentage Discount
 
 #include <iostream>
 using namespace std;
 int main()
 {
-double salary = 6000;
-double percent1 = 5, percent2 = 7, percent3 = 10;
-double contribution1, contribution2, contribution3;
-contribution1 = salary * percent1 / 100;
-contribution2 = salary * percent2 / 100;
-contribution3 = salary * percent3 / 100;
-cout << "Amount of contribution at " << percent1 << "% is $" <<
-contribution1 << endl;
-cout << "Amount of contribution at " << percent2 << "% is $" <<
-contribution2 << endl;
-cout << "Amount of contribution at " << percent3 << "% is $" <<
-contribution3 << endl;
+double regularPrice, percentDiscount, discountAmount,
+reducedPrice;
+regularPrice = 59.95;
+percentDiscount = 0.2;
+discountAmount = regularPrice * percentDiscount;
+reducedPrice = regularPrice - discountAmount;
+cout << "The discount amount is $" << discountAmount << endl;
+cout << "The discounted price is $" << reducedPrice;
 
   return 0;
 }
