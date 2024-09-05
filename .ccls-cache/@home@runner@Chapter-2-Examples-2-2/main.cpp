74525720dd7359a4b6ec3643d7ed61db411a90a3
zwhippo9 <zwhippo9@gmail.com>
2024-09-05 20:26:33 +0000

    Final Check

---
+++ .ccls-cache/@home@runner@Chapter-2-Examples-2-2/main.cpp
@@ -0,0 +1,25 @@
+//This program calculates and displays the several values 
+// and various percentages for those values
+// Calculating Percentages
+
+#include <iostream>
+using namespace std;
+int main()
+{
+double salary = 6000;
+double percent1 = 5, percent2 = 7, percent3 = 10;
+double contribution1, contribution2, contribution3;
+contribution1 = salary * percent1 / 100;
+contribution2 = salary * percent2 / 100;
+contribution3 = salary * percent3 / 100;
+cout << "Amount of contribution at " << percent1 << "% is $" <<
+contribution1 << endl;
+cout << "Amount of contribution at " << percent2 << "% is $" <<
+contribution2 << endl;
+cout << "Amount of contribution at " << percent3 << "% is $" <<
+contribution3 << endl;
+
+  return 0;
+}
+
+// This is my secon Edit I pray this works too.
