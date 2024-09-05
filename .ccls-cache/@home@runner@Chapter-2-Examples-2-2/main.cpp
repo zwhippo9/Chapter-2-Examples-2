016249511e78f473f403f2211b5c98bcfcfdc0e6
zwhippo9 <zwhippo9@gmail.com>
2024-09-05 20:27:00 +0000

    Final Check before Submit

---
+++ .ccls-cache/@home@runner@Chapter-2-Examples-2-2/main.cpp
@@ -0,0 +1,21 @@
+// This program calculates and displays the discounted value of an item.
+// Percentage Discount
+
+#include <iostream>
+using namespace std;
+int main()
+{
+double regularPrice, percentDiscount, discountAmount,
+reducedPrice;
+regularPrice = 59.95;
+percentDiscount = 0.2;
+discountAmount = regularPrice * percentDiscount;
+reducedPrice = regularPrice - discountAmount;
+cout << "The discount amount is $" << discountAmount << endl;
+cout << "The discounted price is $" << reducedPrice;
+
+  return 0;
+}
+
+
+//This is my edit it should be working. At least I pray it is.
